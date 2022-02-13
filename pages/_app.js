e9a1edeb4e8749958cbe9f4982219fb545f71035
Asim J <asimjawahir123@gmail.com>
2022-02-13 05:28:19 +0300

    My Assets Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
-
-import '../styles/globals.css'
-import Link from 'next/link'
+import "../styles/globals.css";
+import Link from "next/link";
 
 function Marketplace({ Component, pageProps }) {
   return (
@@ -9,25 +8,22 @@ function Marketplace({ Component, pageProps }) {
         <p className="text-4xl font-bold">Ticket Marketplace</p>
         <div className="flex mt-4">
           <Link href="/">
-            <a className="mr-4 text-pink-500">
-              Home
-            </a>
+            <a className="mr-4 text-pink-500">Home</a>
           </Link>
           <Link href="/sell-ticket">
-            <a className="mr-6 text-pink-500">
-              Sell Ticket
-            </a>
+            <a className="mr-6 text-pink-500">Sell Ticket</a>
           </Link>
           <Link href="/masssell">
-            <a className="mr-6 text-pink-500">
-              Mass Sell
-            </a>
+            <a className="mr-6 text-pink-500">Mass Sell</a>
+          </Link>
+          <Link href="/myAssets">
+            <a className="mr-6 text-pink-500">My Assets</a>
           </Link>
         </div>
       </nav>
       <Component {...pageProps} />
     </div>
-  )
+  );
 }
 
-export default Marketplace
+export default Marketplace;
