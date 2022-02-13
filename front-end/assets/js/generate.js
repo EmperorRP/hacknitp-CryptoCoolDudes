6374a1c797f9f99c348b7a4f3fa35b8c69d18cb3
Nevan <nev.kd2832@gmail.com>
2022-02-13 05:52:08 -0500

    added multiple page functionality and designed the pages

---
+++ front-end/assets/js/generate.js
@@ -0,0 +1,10 @@
+let singleticketEl = document.querySelector('#single-ticket');
+let massticketEl = document.querySelector('#mass-ticket');
+
+singleticketEl.addEventListener('click',function(){
+    window.location.href = 'single-ticket.html';
+})
+
+massticketEl.addEventListener('click',function(){
+    window.location.href = 'single-ticket.html';
+})
--- front-end/assets/js/script.js
+++ front-end/assets/js/script.js
@@ -1,3 +1,6 @@
+// Declarations
+let generateBtnEl = document.getElementById('generate-btn');
+let marketplaceBtnEl = document.getElementById('marketplace-btn');
 // Functions
 // Navbar Burger
 function navbarBurger(){
@@ -9,6 +12,15 @@ function navbarBurger(){
         nav.classList.toggle('is-active');
     })
 }
+
+// Generate Button
+generateBtnEl.addEventListener('click',function(){
+    window.location.href = 'generate.html';
+})
+
+marketplaceBtnEl.addEventListener('click',function(){
+    window.location.href = 'marketplace.html';
+})
 
 // Callings
 navbarBurger();
